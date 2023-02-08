8bf14d2045724db688331a87bc31b1007fc11a16
FateSealed40 <lucasbholden@gmail.com>
2023-02-07 18:49:01 -0800

    Update VisionAlignment.java

---
+++ src/main/java/frc/robot/commands/VisionAlignment.java
@@ -0,0 +1,20 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.VisionSubsystem;
+import frc.robot.Constants;
+
+public class VisionAlignment extends CommandBase {
+
+  public VisionAlignment() {}
+
+  @Override
+  public void initialize() {
+  }
+
+  
+
+  @Override
+  public void end(boolean interrupted) {
+  }
+}
