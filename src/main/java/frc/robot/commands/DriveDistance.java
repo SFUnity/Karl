7febd95d1f6d7f85bc9fd1b213598582ea3641f2
MrTinker64 <avi.micah@gmail.com>
2023-02-11 14:15:28 -0800

    Switched to a PIDController rather than all text

--- src/main/java/frc/robot/commands/DriveDistance.java
+++ src/main/java/frc/robot/commands/DriveDistance.java
@@ -4,70 +4,49 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class DriveDistance extends CommandBase {
   private final DriveSubsystem m_drive;
-  private final double m_setpoint;
   private final double m_speed;
-  private double kP = 0.5;
-  private double kI = 0.5;
-  private double kD = 0.1;
-  private static double error;
-  private static double errorSum;
-  private static double lastTimestamp;
-  private static double lastError;
-  private static double errorRate;
-  private static double dt;
+  private final PIDController pidController;
 
   /**
    * Creates a new DriveDistance.
    *
-   * @param feet The number of feet the robot will drive
-   * @param drive  The drive subsystem on which this command will run
+   * @param setpoint The number of feet the robot will drive
+   * @param drive    The drive subsystem on which this command will run
    */
-  public DriveDistance(double feet, DriveSubsystem drive) {
-    m_setpoint = feet;
-    m_speed = (kP * error + kI * errorSum + kD * errorRate) / 12;
+  public DriveDistance(double setpoint, DriveSubsystem drive) {
     m_drive = drive;
+    pidController = new PIDController(0.5, 0.5, 0.1);
+    pidController.setSetpoint(setpoint);
+    m_speed = pidController.calculate(m_drive.getAverageEncoderDistance());
     addRequirements(m_drive);
   }
 
   @Override
   public void initialize() {
     m_drive.resetEncoders();
-    errorSum = 0;
-    lastTimestamp = Timer.getFPGATimestamp();
-    lastError = 0;
+    pidController.reset();
     m_drive.tankDrive(m_speed, m_speed);
   }
 
   @Override
   public void execute() {
-    error = m_setpoint - m_drive.getAverageEncoderDistance();
-    dt = Timer.getFPGATimestamp() - lastTimestamp;
-
-    if (Math.abs(error) < 1) {
-      errorSum += error * dt;
-    }
-    errorRate = (error - lastError);
-
     m_drive.tankDrive(m_speed, m_speed);
-
-    lastTimestamp = Timer.getFPGATimestamp();
-    lastError = error;
   }
 
   // Sets speed to 0 when ended
   @Override
   public void end(boolean interrupted) {
-      m_drive.tankDrive(0, 0);
+    m_drive.tankDrive(0, 0);
   }
 
   @Override
   public boolean isFinished() {
-    return Math.abs(m_drive.getAverageEncoderDistance()) >= m_setpoint;
+    return false;
   }
 }
