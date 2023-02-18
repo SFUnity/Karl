2848383f23ecf4d93d5254d7f1954b8a5087a837
MrTinker64 <avi.micah@gmail.com>
2023-02-18 14:09:08 -0800

    Added a PlacePiece command

---
+++ src/main/java/frc/robot/commands/PlacePiece.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.ArmSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+
+public class PlacePiece extends CommandBase {
+    private final ArmSubsystem m_arm;
+
+    /**
+     * Creates a new DefaultDrive.
+     *
+     * @param subsystem The drive subsystem this command wil run on.
+     */
+    public PlacePiece(ArmSubsystem subsystem) {
+        m_arm = subsystem;
+        addRequirements(m_arm);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        if (Constants.ArmConstants.lastGamePiece == Constants.ArmConstants.CONE) {
+            m_arm.cubeInConeOut();
+        } else if (Constants.ArmConstants.lastGamePiece == Constants.ArmConstants.CUBE) {
+            m_arm.coneInCubeOut();
+        } else {
+            m_arm.holdPiece();
+        }
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
