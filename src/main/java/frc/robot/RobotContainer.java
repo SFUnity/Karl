2f52ec065ffb12006b43e5665c3d05c220a755e0
SFUnity <sfunityrobotics@gmail.com>
2023-04-19 13:14:22 -0700

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -55,8 +55,8 @@ public class RobotContainer {
         private final Command kDriveForward2 = new DriveDistance(m_robotDrive, 2, 0.5);
         private final Command kTurnAround = new TurnToAngle(m_robotDrive, 180);
         private final Command kTurn90 = new TurnToAngle(m_robotDrive, 90);
-        private final Command kSide = Commands.sequence(kPlacePiece, kDriveBack8, kTurn90);
-        // private final Command kAutoDock = Commands.sequence(kPlacePiece, kTurnAround, kDriveForward2, kPIDDock);
+        // private final Command kSide = Commands.sequence(kPlacePiece, kDriveBack8, kTurn90);
+        private final Command kAutoDock = Commands.sequence(kPlacePiece, kTurnAround, kDriveForward2, kPIDDock);
         private final Command kNothing2 = Commands.sequence();
         private final SendableChooser<Command> m_chooser = new SendableChooser<>();
 
@@ -99,11 +99,11 @@ public class RobotContainer {
                 m_chooser.addOption("middle", kMiddleAuto);
                 m_chooser.addOption("bump", kBumpAuto);
                 m_chooser.addOption("knight dock", kPIDDock);
-                m_chooser.addOption("side", kSide);
+                // m_chooser.addOption("side", kSide);
                 m_chooser.addOption("nothing2", kNothing2);
                 m_chooser.addOption("place cone", kPlacePiece);
                 m_chooser.addOption("drive back 8ft", kDriveBack8);
-                // m_chooser.addOption("auto dock", kAutoDock);
+                m_chooser.addOption("auto dock", kAutoDock);
 
                 m_chooser2.addOption("cube", CUBE);
                 m_chooser2.addOption("cone", CONE);
